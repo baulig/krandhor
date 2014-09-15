04ffab3a3debca8b83e4561abc0a923feba87a66
Martin Baulig <martin.baulig@xamarin.com>
2014-09-15 16:50:30 +0200

    Add Console app.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestWCF
+{
+	class Program
+	{
+		MyServiceClient client;
+		AutoResetEvent asyncEvent;
+
+		static void Main(string[] args)
+		{
+			var program = new Program();
+			program.Run();
+		}
+
+		Program ()
+		{
+			client = ServiceClientHelper.CreateServiceClient();
+			asyncEvent = new AutoResetEvent(false);
+			client.TestByRefCompleted += TestByRefCompleted;
+		}
+
+		void Run ()
+		{
+			var result = new ManualResetEvent(false);
+			int test = 8;
+			client.TestByRefAsync(test);
+			asyncEvent.WaitOne();
+		}
+
+		void TestByRefCompleted(object sender, TestByRefCompletedEventArgs e)
+		{
+			Console.WriteLine("TEST: {0}", e.test);
+			asyncEvent.Set();
+		}
+	}
+}
