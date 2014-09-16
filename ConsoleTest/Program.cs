e2652a3cf55f922b4ca456c5f6990667f95e1598
Martin Baulig <martin.baulig@xamarin.com>
2014-09-16 23:14:34 +0200

    This was just a test to make sure the sync API still works.
    Revert "That's the sync proxy that's generated by the non-silverlight svcutil.exe"
    
    This reverts commit dc55925a6ba2f885c8a06426ff3db4fd4d069581.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -25,11 +25,9 @@ namespace TestWCF
 		Program ()
 		{
 			client = ServiceClientHelper.CreateServiceClient ();
-			#if ASYNC
 			asyncEvent = new AutoResetEvent (false);
 			client.TestOutCompleted += TestOutCompleted;
 			client.TestByRefCompleted += TestByRefCompleted;
-			#endif
 		}
 
 		void Test ()
@@ -46,14 +44,12 @@ namespace TestWCF
 			}
 		}
 
-		#if ASYNC
 		void Run ()
 		{
 			int test = 8;
 			client.TestByRefAsync(2048, test);
 			client.TestOutAsync ();
 			asyncEvent.WaitOne ();
-
 		}
 
 		void TestOutCompleted (object sender, TestOutCompletedEventArgs e)
@@ -66,16 +62,5 @@ namespace TestWCF
 			Console.WriteLine ("TEST: {0} {1}", e.test, e.time);
 			asyncEvent.Set ();
 		}
-		#else
-		void Run ()
-		{
-			int test = 8;
-			DateTime time;
-			client.TestByRef (2048, ref test, out time);
-			Console.WriteLine ("TEST: {0} {1}", test, time);
-			test = client.TestOut (out time);
-			Console.WriteLine ("TEST #1: {0} {1}", test, time);
-		}
-		#endif
 	}
 }
